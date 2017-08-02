0b6ef14dba393a2655541faa20de778e97d66e86
User <lepetitjan@icloud.com>
2017-08-02 06:47:37 -0700

    Initial commit

---
+++ helpers/exceptions.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+	genesisPublicKey: {
+		mainnet: '4c1170a3edb03f961e5e3f7cedcd25563f0a46ec4aa3342715d09c47b398ea19',
+		testnet: '0daee950841005a3f56f6588b4b084695f0d74aaa38b21edab73446064638552'
+	},
+	rounds: {
+	},
+	senderPublicKey: [
+	],
+	signatures: [
+	],
+	multisignatures: [
+	],
+	balance: [
+	],
+	votes: [
+	]
+};
